0d77795986468b1845715c9976d4f74a7db563b3
Thai Pangsakulyanont <dtinth@spacet.me>
2020-07-12 20:36:41 +0700

    Add screen-capture tool

---
+++ features/screen-capture.js
@@ -0,0 +1,11 @@
+App.registerFeature('screen-capture', {
+  template: html`
+    <div id="screen-capture" class="p-1">
+      TODO
+    </div>
+  `,
+  css: css`
+    #screen-capture {
+    }
+  `,
+})
